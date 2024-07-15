32d7c0558b72738d67f300e82c169c90715be791
HoffmannLuca <luca.hoffmann1000@gmail.com>
2024-07-16 00:02:46 +0200

    add seeder for initial admin user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,21 +3,27 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
+    use WithoutModelEvents;
+
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $email = env('DB_ADMIN_EMAIL', 'test@example.com');
+        $user = User::where('email', $email)->first();
+        if(!$user){
+            User::create([
+                'name' => 'ADMIN',
+                'email' => $email,
+                'password' => Hash::make('password'),
+            ]);
+        }
     }
 }
